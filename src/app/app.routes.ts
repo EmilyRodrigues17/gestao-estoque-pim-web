31b0eba9c5751eab923b1007e63e92502b7bac12
Emily Fogaca <emilyrodrigues17@hotmail.com>
2026-05-01 18:03:47 -0400

    feat: add rotas novas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,27 @@
 import { Routes } from '@angular/router';
 import { unsavedChangesGuard } from './core/guards/unsaved-changes-guard';
+import { authGuard } from './core/auth/auth.guard';
+import { roleGuard } from './core/guards/role.guard';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'app/dashboard',
+        redirectTo: 'login',
         pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./features/login/login').then(m => m.Login),
+    },
+    {
+        path: 'trocar-senha',
+        loadComponent: () => import('./features/trocar-senha/trocar-senha').then(m => m.TrocarSenha),
+        canActivate: [authGuard],
     },
     {
         path: 'app',
         loadComponent: () => import('./layout/main-layout/main-layout').then(m => m.MainLayout),
+        canActivate: [authGuard],
         children: [
             {
                 path: 'dashboard',
@@ -21,17 +33,35 @@ export const routes: Routes = [
                 canDeactivate: [unsavedChangesGuard]
             },
             {
+                path: 'insumos/:id',
+                loadComponent: () => import('./features/insumos/insumo-details/insumo-details').then(m => m.InsumoDetails),
+            },
+            {
                 path: 'movimentacoes',
                 loadComponent: () => import('./features/movimentacoes/movimentacoes').then(m => m.Movimentacoes),
             },
             {
+                path: 'movimentacoes/nova',
+                loadComponent: () => import('./features/movimentacoes/nova-movimentacao/nova-movimentacao').then(m => m.NovaMovimentacao),
+                canActivate: [roleGuard],
+                data: { roles: ['almoxarife'] },
+            },
+            {
                 path: 'categorias',
                 loadComponent: () => import('./features/categorias/categorias').then(m => m.Categorias),
+                canActivate: [roleGuard],
+                data: { roles: ['almoxarife'] },
+            },
+            {
+                path: 'usuarios',
+                loadComponent: () => import('./features/usuarios/usuarios').then(m => m.Usuarios),
+                canActivate: [roleGuard],
+                data: { roles: ['adm'] },
             }
         ]
     },
     {
         path: '**',
-        redirectTo: 'dashboard'
+        redirectTo: 'login'
     }
 ];
